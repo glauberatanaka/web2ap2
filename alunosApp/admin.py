4b037459096cde121cc144a887271c926b53f577
glauberatanaka <glauberatanaka@gmail.com>
2019-04-18 16:22:38 -0400

    cria models e migrations

--- alunosApp/admin.py
+++ alunosApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from alunosApp.models import Curso, Aluno
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Aluno)
--- alunosApp/models.py
+++ alunosApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    curso_nome = models.CharField(max_length=100, unique=True)
+    bloco = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.curso_nome
+
+class Aluno(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE, verbose_name="curso")
+    nome = models.CharField(max_length=256)
+    idade = models.IntegerField()
+    semestre = models.IntegerField()
+    rga = models.CharField(max_length=12, unique=True)
+
+    def __str__(self):
+        return self.nome
