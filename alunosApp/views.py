5185f38fc5e768b48fe1595561a2edf88b81cd85
glauberatanaka <glauberatanaka@gmail.com>
2019-04-18 16:43:18 -0400

    cria listar

--- alunosApp/views.py
+++ alunosApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from alunosApp.models import Curso, Aluno
 # Create your views here.
 
 def index(request):
-    test_dict = {'testTemplateTag':'Sou uma template tag!'}
-    return render(request, 'alunosApp/index.html', context=test_dict)
+    alunos_list = Aluno.objects.order_by('nome')
+    alunos_dict = {'alunos': alunos_list}
+    return render(request, 'alunosApp/index.html', context=alunos_dict)
